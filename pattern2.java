eb528da9f340968d4f69e11afb8ec1f2d8dadbab
Prem192002 <premsingh192002@gmail.com>
2023-05-04 12:09:52 +0530

    third commit

--- pattern2.java
+++ pattern2.java
@@ -2,19 +2,9 @@ public class pattern2 {
     public static void main(String[] args){
 
         for(int i=1;i<=4;i++){
-            for(int j=1;j<=6;j++){
-
-                if(i==1||i==4||j==1||j==6){
-                    System.out.print("*");
-                }
-                else{
-                System.out.print(" ");
-                }
-            
+            for(int j)
         }
-        System.out.println();
 
     }
     
-}
 }
