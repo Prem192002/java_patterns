dbe77ced1a78ce2766f209113bef94f57be5dd78
Prem192002 <premsingh192002@gmail.com>
2023-05-05 14:05:13 +0530

    fourth commit

--- pattern3.java
+++ pattern3.java
@@ -4,7 +4,7 @@ public class pattern3 {
 
         for(int i=1;i<=4;i++){
 
-            for(int j=1; j<=i;j++){
+            for(int j=1; j<=(i);j=j+1){
 
                 System.out.print("*");
             }
---
+++ pattern4.java
@@ -0,0 +1,20 @@
+public class pattern4 {
+
+    public static void main(String[] args){
+
+        int r = 4;
+
+        for(int i=1;i<=r; i++){
+
+            for(int j=1;j<=r-i;j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k<=2*i-1;k++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+    
+}
